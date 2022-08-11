793f224a11a028473997a3178fcc696e3c761084
github akito-10 <akt.fukuda@gmail.com>
2022-08-11 16:01:22 +0900

    Brgin project!

---
+++ src/main/kotlin/com/akito/springwebapimysql/SpringWebApiMySqlApplication.kt
@@ -0,0 +1,11 @@
+package com.akito.springwebapimysql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebApiMySqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebApiMySqlApplication>(*args)
+}
